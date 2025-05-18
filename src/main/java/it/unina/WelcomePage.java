bc23fdc41098fc3a5e3e8a7f557407a2a0c302fe
Sderr12 <andrea.sidero6@gmail.com>
2025-05-18 23:56:50 +0200

    Add a WelcomePage with register and login pane, add a transition animation

--- src/main/java/it/unina/WelcomePage.java
+++ src/main/java/it/unina/WelcomePage.java
@@ -3,6 +3,7 @@ package it.unina;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,13 +11,14 @@ import java.io.IOException;
 public class WelcomePage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WelcomePage.class.getResource("/it/unina/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(WelcomePage.class.getResource("/it/unina/WelcomeCard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
 
         scene.getStylesheets().add(WelcomePage.class.getResource("/it/unina/css/WelcomePage.css").toExternalForm());
 
         stage.setTitle("");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/it/unina/controller/WelcomeController.java
+++ src/main/java/it/unina/controller/WelcomeController.java
@@ -1,14 +1,55 @@
 package it.unina.controller;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class WelcomeController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Just a simple Prove");
+    @FXML private VBox loginPane;
+
+    @FXML private VBox registerPane;
+
+
+
+    @FXML private void initialize() {
+        loginPane.setVisible(true);
+        loginPane.setOpacity(1.0);
+        registerPane.setVisible(false);
+        registerPane.setOpacity(0.0);
+    }
+
+    @FXML private void onLogin() {
+        System.out.println("OK!");
+    }
+
+    @FXML private void onRegister() {
+        System.out.println("OK!");
+    }
+
+    @FXML private void showRegister() {
+        animateSwitch(loginPane, registerPane);
+    }
+
+    @FXML private void showLogin() {
+        animateSwitch(registerPane, loginPane);
+    }
+
+    private void animateSwitch(VBox from, VBox to) {
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(300), from);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+        fadeOut.setOnFinished(e -> {
+            from.setVisible(false);
+
+            to.setVisible(true);
+            FadeTransition fadeIn = new FadeTransition(Duration.millis(300), to);
+            fadeIn.setFromValue(0.0);
+            fadeIn.setToValue(1.0);
+            fadeIn.play();
+        });
+
+        fadeOut.play();
     }
 }
