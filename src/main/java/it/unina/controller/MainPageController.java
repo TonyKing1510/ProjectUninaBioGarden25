0c25940176f4122c0619248a77f54d2d184160d5
Sderr12 <andrea.sidero6@gmail.com>
2025-05-30 01:58:09 +0200

    feat:add mainpage.css and MainPage.fxml

---
+++ src/main/java/it/unina/controller/MainPageController.java
@@ -0,0 +1,10 @@
+package it.unina.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class MainPageController {
+
+}
