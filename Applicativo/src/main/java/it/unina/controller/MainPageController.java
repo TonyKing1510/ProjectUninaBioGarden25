2921ed32cec4b252332f0765ab240b086e739169
Sderr12 <andrea.sidero6@gmail.com>
2025-06-03 01:43:19 +0200

    feat: add Colture view

--- Applicativo/src/main/java/it/unina/controller/MainPageController.java
+++ Applicativo/src/main/java/it/unina/controller/MainPageController.java
@@ -1,10 +1,33 @@
 package it.unina.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class MainPageController {
+
+  @FXML private Button plantManagementButton;
+  @FXML private Button userManagementButton;
+  @FXML private Button statisticsButton;
+
+  @FXML private BorderPane borderPane;
+
+  @FXML private void goToPlantManagement() {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/it/unina/ColtureView.fxml"));
+      Parent node = loader.load();
+      node.getStylesheets().add(getClass().getResource("/it/unina/css/coltureview.css").toExternalForm());
+      borderPane.setCenter(node);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
